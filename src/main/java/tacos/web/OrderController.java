206bff9d697e28e76965564d21b6e6fc728e1015
Mihail Calaras <mihail.calaras@reigncode.dev>
2020-10-13 22:16:48 +0300

    Add all

---
+++ src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,32 @@
+package tacos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.domain.TacoOrder;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("tacoOrder", new TacoOrder());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder tacoOrder, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+        log.info("Order submitted: " + tacoOrder);
+        return "redirect:/";
+    }
+
+}
